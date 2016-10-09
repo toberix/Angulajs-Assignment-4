f435e3ccdc670e61f4529cedf17eef40c0fe8ccc
Fatumo Oluwatobi Ifeoluwa <tfatumo@emzorlan.com>
2016-10-09 13:54:50 +0100

    New

---
+++ js/components/categories.component.js
@@ -0,0 +1,10 @@
+
+
+
+<!--<ul>
+  <li ng-repeat="item in ctrl.items">
+    <b>{{ item.name }}</b> [{{ item.short_name }}]<br />
+    {{ item.description }}
+  </li>
+</ul>-->
+<items list="ctrl.items"></items>
